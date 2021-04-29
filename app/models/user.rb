a432117e96a649aa6d8609217a25d935b8423e3b
takuya-kun <79629910+takuya-kun@users.noreply.github.com>
2021-04-29 11:23:10 +0900

    新規登録機能、ログアウト機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :last_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/ }
+  validates :ruby_last_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :ruby_first_name, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
+  validates :birthday, presence: true
+
 end
