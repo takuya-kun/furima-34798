e22c22052c29a1eb574db6ae80fc17c15654a109
takuya-kun <79629910+takuya-kun@users.noreply.github.com>
2021-05-12 19:04:10 +0900

    商品出品機能(画像以外)の実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,15 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :image
+    validates :product_name
+    validates :text
+    validates :category_id
+    validates :condition_id
+    validates :burden_id
+    validates :prefecture_id
+    validates :shipping_day_id
+    validates :price
+  end
 end
