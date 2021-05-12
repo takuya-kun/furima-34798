8c4f27d59d822d4a5fcf38ec502b24dfc7e517c0
takuya-kun <79629910+takuya-kun@users.noreply.github.com>
2021-05-12 21:01:08 +0900

    商品出品機能のActiveHash導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,15 +1,26 @@
 class Item < ApplicationRecord
   belongs_to :user
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :burden
+  belongs_to :prefecture
+  belongs_to :shipping_day
+
   with_options presence: true do
     validates :image
     validates :product_name
     validates :text
-    validates :category_id
-    validates :condition_id
-    validates :burden_id
-    validates :prefecture_id
-    validates :shipping_day_id
+
+    with_options numericality: { other_than: 1 } do
+      validates :category_id
+      validates :condition_id
+      validates :burden_id
+      validates :prefecture_id
+      validates :shipping_day_id
+    end
+    
     validates :price
   end
 end
