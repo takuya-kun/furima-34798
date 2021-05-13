1559c67a1c71ef44789f9663220c139728ef1333
takuya-kun <79629910+takuya-kun@users.noreply.github.com>
2021-05-13 17:51:45 +0900

    商品出品機能の指摘点修正_2回目

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -106,6 +106,12 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
+
+      it 'userが存在しないと保存できないこと' do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include('User must exist')
+      end
     end
   end
 end
