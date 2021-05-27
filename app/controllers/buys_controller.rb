0580db3c189ec510bf06b801c92fc92ba019a44b
takuya-kun <79629910+takuya-kun@users.noreply.github.com>
2021-05-27 17:38:59 +0900

    クレジットカード決算機能の追加

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -7,8 +7,10 @@ class BuysController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @buy = Buy.new(buy_params)
-    if @buy.save
-      redirect_to root_path
+    if @buy.valid?
+      buy_item
+      @buy.save
+      return redirect_to root_path
     else
       render :index
     end
@@ -16,6 +18,16 @@ class BuysController < ApplicationController
 
   private
   def buy_params
-    params.require(:buy).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:buy).permit(:postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number).merge(token: params[:token], user_id: current_user.id, item_id: params[:item_id])
   end
+
+  def buy_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+      amount: buy_params[:price],
+      card: buy_params[:token],
+      currency: 'jpy'
+    )
+  end
+
 end
--- app/models/buy.rb
+++ app/models/buy.rb
@@ -2,4 +2,6 @@ class Buy < ApplicationRecord
   has_one :address
   belongs_to :user
   belongs_to :item
+
+  attr_accessor :token
 end
