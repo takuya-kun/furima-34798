dc89ca627dce8d161d83a71f81df2287fe5d7cd1
takuya-kun <79629910+takuya-kun@users.noreply.github.com>
2021-05-27 14:35:54 +0900

    buysコントローラーの編集、購入画面への遷移

--- app/controllers/buys_controller.rb
+++ app/controllers/buys_controller.rb
@@ -1,2 +1,21 @@
 class BuysController < ApplicationController
+
+  def index
+    @item = Item.find(params[:item_id])
+  end
+
+  def create
+    @item = Item.find(params[:item_id])
+    @buy = Buy.new(buy_params)
+    if @buy.save
+      redirect_to root_path
+    else
+      render :index
+    end
+  end
+
+  private
+  def buy_params
+    params.require(:buy).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
